96ac93d6c4a5863b810105142f576605ce089b7e
icraig1205 <126037875+icraig1205@users.noreply.github.com>
2026-03-24 20:43:08 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+// Create the map centered on Ontario
+var map = L.map('map').setView([44.0, -79.5], 6);
+
+// Basemap
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 18
+}).addTo(map);
+
+var heatLayer;
+
+// Load GeoJSON and build heatmap
+function loadYear(year) {
+    fetch(`data/${year}.geojson`)
+        .then(response => response.json())
+        .then(data => {
+            if (heatLayer) {
+                map.removeLayer(heatLayer);
+            }
+
+            // Convert GeoJSON to heatmap points
+            var heatPoints = data.features.map(f => {
+                var lat = f.geometry.coordinates[1];
+                var lon = f.geometry.coordinates[0];
+                var weight = f.properties.rate_postings || 0;
+                return [lat, lon, weight];
+            });
+
+            heatLayer = L.heatLayer(heatPoints, {
+                radius: 25,
+                blur: 15,
+                maxZoom: 10
+            }).addTo(map);
+        });
+}
+
+// Load default year
+loadYear("2025");
+
+// Dropdown listener
+document.getElementById("yearSelect").addEventListener("change", function () {
+    loadYear(this.value);
+});
